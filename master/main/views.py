ee4926126e107b5e18c00ac67f36624fb5c6fa53
illud <42035117+illud@users.noreply.github.com>
2018-12-18 09:13:59 -0500

    django-json-fetch

---
+++ master/main/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import requests, urllib
+import json
+# Create your views here.
+def index(request):
+    url = ("https://facebook.github.io/react-native/movies.json")
+    response = urllib.request.urlopen(url)
+    data = json.loads(response.read())
+    print (data)
+    data_title = [];
+    data_id = [];
+    for i in data['movies']:
+        data_id.append(i['id'])
+        data_title.append(i['id'] + " " + i['title'] + " " + i['releaseYear'])
+        print (i['title'])
+    return render(request, 'index.html', {'dara': data_title})
