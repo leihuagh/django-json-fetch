1df69ad7e50b57a373c61d4f2cefd46c2143bc46
illud <illudharpyja@gmail.com>
2018-12-31 18:57:13 -0500

    Added home page in path

--- master/master/urls.py
+++ master/master/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from main import views
 urlpatterns = [
+    path('', views.index, name="home"),
     path('main/', include('main.urls')),
     path('admin/', admin.site.urls),
 ]
